a170096e2c3872093a5bc046526d269dc7938e7c
Chandrashekar Kuthyar <ckuthyar@gmail.com>
2024-01-07 21:43:01 +0530

    String Splicing

--- 8.py
+++ 8.py
@@ -1,6 +1,11 @@
 s1="I am a student of CS in SMVITM Bantakal"
 list1=s1.split(" ")
 tuple1=s1.partition("in")
+print(list1)
+print(tuple1)
 
+s1="abcdefghijklmnopqrstuvwxyz"
+list1=s1.split("m")
+tuple1=s1.partition("m")
 print(list1)
 print(tuple1)
---
+++ 9.py
@@ -0,0 +1,18 @@
+s1="This is Python exam"
+print(s1[0:6])
+print(s1[0:5])
+print(s1[0:4])
+print(s1[0:3])
+print(s1[0:2])
+print(s1[0:1])
+
+print(s1[-5])
+print(s1[-4])
+print(s1[-3])
+
+print(s1[:6])
+print(s1[5:])
+
+print(s1[::-1])
+
+print(s1[5])
