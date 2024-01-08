cce3fb38bb4da056a724b393bfd15cc09fa59812
Chandrashekar Kuthyar <ckuthyar@gmail.com>
2024-01-08 05:57:26 +0530

    isupper(), islower()

--- 11.py
+++ 11.py
@@ -1,5 +1,6 @@
 s1="This is Python exam"
 print(s1.rjust(100))
 print(s1.rjust(50))
+
 print(s1.center(100))
 print(s1.center(50))
---
+++ 12.py
@@ -0,0 +1,14 @@
+s1="HELLO"
+s2="Hello"
+s3="hello"
+print(s1.isupper())
+print(s1.islower())
+print()
+
+print(s2.isupper())
+print(s2.islower())
+print()
+
+print(s3.isupper())
+print(s3.islower())
+print()
